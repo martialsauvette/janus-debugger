c810b879c7d0734764b36cfd6136f402d98b92a3
GitHub Action <action@github.com>
2021-02-18 13:33:04 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/martialsauvette/janusdebugger/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.martialsauvette.janusdebugger.services
+
+import com.github.martialsauvette.janusdebugger.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
